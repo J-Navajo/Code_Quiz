85a687d4750d8ee363f092804286b791a939d6f0
J-Navajo <jnnfr.stnly@gmail.com>
2019-12-07 16:59:04 -0700

    created this file because instructions said so.

---
+++ questions.js
@@ -0,0 +1,44 @@
+
+const questions = [
+    {
+        title: "Question 1",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 3"
+    },
+    {
+        title: "Question 2",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 2"
+    },
+    {
+        title: "Question 3",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 1"
+    },
+    {
+        title: "Question 4",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 2"
+    },
+    {
+        title: "Question 5",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 4"
+    },
+    {
+        title: "Question 6",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 3"
+    },
+    {
+        title: "Question 7",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 1"
+    },
+    {
+        title: "Question 8",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4"],
+        answer: "choice 4"
+    },
+
+]
