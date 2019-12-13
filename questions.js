a0e34bbbfb0c277ea153912adc2b7d5b81ce6b71
J-Navajo <jnnfr.stnly@gmail.com>
2019-12-12 18:35:21 -0700

    start button is working meaning it starts the timer

--- questions.js
+++ questions.js
@@ -1,3 +1,57 @@
+$(document).ready(function(){
+const timerslotEl = $("#timerslot")
+const mainEl = document.getElementById("main");
+const qholder = document.querySelector(".questionholder");
+
+
+timerslotEl.hide();
+let secondsLeft = 120;
+
+function setTime() {
+    timerslotEl.show();
+    const timerInterval = setInterval(function(){
+        secondsLeft--;
+        timerslotEl.text(secondsLeft + " time left.");
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+function startQuiz() {
+    document.getElementById("quiz1").innerHTML = "Answer the following questions in this quiz within the time limit. Keep in mind that incorrect answers will come with a penalty and will impact your score/time by nine seconds.";
+    
+    setTime();
+        elephant();
+}
+
+function elephant() {
+    // questions.forEach(element => {
+        
+    // });
+    for (i = 0; i < questions.length; i++) {
+        var qtitle = document.createElement('h2');
+        qtitle.innerHTML = questions[i].title;
+        // $(".qholder").append("foo");
+        qholder.append(qtitle);
+        // document.qholder.appendChild(qtitle);
+
+        console.log(questions[i].title);
+    }
+}
+
+function sendMessage() {
+    timerslotEl.textContent = " ";
+
+    // const textEl = document.createElement("text");
+
+    h2El.textContent = "End of Quiz"
+    mainEl.appendChild(h2El);
+}
+
+
+
 
 const questions = [
     {
@@ -42,3 +96,5 @@ const questions = [
     },
 
 ]
+$("#startQuiz").click(startQuiz);
+});
